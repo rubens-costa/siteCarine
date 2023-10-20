f67a331676297150301119c5d75c6d449043b979
rubens-costa <rubens.pcosta@hotmail.com>
2023-10-20 18:41:19 -0300

    Atualização das rotas 3 e resumo da descrição de cursos

--- assets/js/habilidades.js
+++ assets/js/habilidades.js
@@ -2,10 +2,10 @@ const skills = document.querySelectorAll('.skill-box');
 const descricao = document.querySelector('#divDescricao');
 const sobreSkill = [
 
-    '<p>Palestra 06/05/2021</p> <br> <p>PubMed: Busca avançada de evidências científicas – Para Grupo de Estudo e Pesquisa em Fisiologia e Envelhecimento (GEPEFE) da UNIVASF.</p> <br>',
-    '<p>Palestra 11/05/2023</p> <br> <p>Instrumentos para Avaliação Multidimensional do Adulto Idoso – Semana Acadêmica UNIP.</p> <br>',
-    '<p>Curso 14/07/2023</p> <br> <p>Pesquisa em bases de dados em saúde (Em parceria com Iara Leal – Organização LABIAFH UPE).</p> <br>',
-    '<p>Curso 21/07/2023</p> <br> <p>Pesquisa em bases de dados em saúde (Em parceria com Iara Leal – Organização LABIAFH UPE).</p> <br>',
+    '<p>PALESTRA 06/05/2021</p> <p>PubMed: Busca avançada de evidências científicas – Para Grupo de Estudo e Pesquisa em Fisiologia e Envelhecimento (GEPEFE) da UNIVASF.</p> <br>',
+    '<p>PALESTRA 11/05/2023</p> <p>Instrumentos para Avaliação Multidimensional do Adulto Idoso – Semana Acadêmica UNIP.</p> <br>',
+    '<p>CURSO 14/07/2023</p> <p>Pesquisa em bases de dados em saúde (Em parceria com Iara Leal – Organização LABIAFH UPE).</p> <br>',
+    '<p>CURSO 21/07/2023</p> <p>Pesquisa em bases de dados em saúde (Em parceria com Iara Leal – Organização LABIAFH UPE).</p> <br>',
 ]
 const dataInicio = [
     (new Date(2021, 10, 1)).getTime(), // 0
